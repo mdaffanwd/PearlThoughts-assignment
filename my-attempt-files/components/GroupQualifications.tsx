d4b321c0c3ee48aab9c1d161233205c7fb787191
mdaffanwd <mohammedaffan2305@gmail.com>
2025-07-11 20:43:03 +0530

    solved all linting errors!✌🏻

--- my-attempt-files/components/GroupQualifications.tsx
+++ my-attempt-files/components/GroupQualifications.tsx
@@ -1,4 +1,4 @@
-import { Qualification } from "@/types/teacher";
+import { Qualification } from "@/my-attempt-files/types/teacher";
 import QualificationsTable from "./QualificationsTable";
 
 export default function GroupQualifications({ qualifications }: { qualifications: Qualification[] }) {
